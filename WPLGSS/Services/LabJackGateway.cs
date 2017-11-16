dcb70293dee99769fab81fba3bee4263324968e7
Zuodian Hu <zhu48@wisc.edu>
2017-11-16 17:12:31 -0600

    LabJack class initial commit.

---
+++ WPLGSS/Services/LabJackGateway.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LabJack;
+
+namespace WPLGSS.Services
+{
+    public class LabJackGateway
+    {
+        int LJhandle = 0;
+
+        public int OpenLabJack()
+        {
+            LJM.OpenS("ANY", "ANY", "ANY", ref LJhandle);
+
+            return 0;
+        }
+
+        public byte[] GetAnalogData(int Channel)
+        {
+            byte[] Data = new byte[14];
+            int NumBytes = 0;
+
+            if (LJhandle == 0) return Data;
+
+            LJM.ReadRaw(LJhandle, Data, NumBytes);
+
+            return Data;
+        }
+
+        public void SetAnalogData(byte[] Data, int NumBytes)
+        {
+            LJM.WriteRaw(LJhandle, Data, NumBytes);
+        }
+    }
+}
