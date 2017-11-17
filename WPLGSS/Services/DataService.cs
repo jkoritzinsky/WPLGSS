534ac14b8380ba14e615e3e2393d3ad6420c5d3d
Zuodian Hu <zhu48@wisc.edu>
2017-11-17 15:48:52 -0600

    Post data grabbing to UI thread. Changed to using doubles for interfacing with LabJack.

--- WPLGSS/Services/DataService.cs
+++ WPLGSS/Services/DataService.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.Composition;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using WPLGSS.Models;
 
 namespace WPLGSS.Services
@@ -15,16 +16,38 @@ namespace WPLGSS.Services
         public event EventHandler<StatusChangedEventArgs> ArduinoConnectionChanged;
         public event EventHandler<ChannelValueUpdatedEventArgs> ChannelValueUpdated;
 
-        private LabJackGateway LJ;
+        private LabJackGateway LJ = new LabJackGateway();
         private System.Timers.Timer SampleTimer;
+        private IConfigService config;
 
-        private byte[] dataIn;
-        private byte[] dataOut;
+        private double[] dataIn;
+        private double[] dataOut;
+
+        [ImportingConstructor]
+        public DataService(IConfigService config)
+        {
+            this.config = config;
+        }
 
         private void Sample(object source, System.Timers.ElapsedEventArgs e)
         {
-            LJ.SetAnalogData(dataOut, 2);
-            dataIn = LJ.GetAnalogData(0);
+            LJ.SetAnalogData(dataOut);
+            dataIn = LJ.GetAnalogData();
+            DateTime SampleTime = DateTime.Now;
+
+            SynchronizationContext.Current.Post( _ => {
+                foreach (var chan in config.Config.Channels)
+                {
+                    if (chan is InputChannel input && chan.Source == ChannelSource.LabJack)
+                    {
+                        ChannelValueUpdated?.Invoke(
+                            this,
+                            new ChannelValueUpdatedEventArgs(chan, input.ScalingFunction(dataIn[input.ChannelId]), SampleTime)
+                        );
+                    }
+                }
+            }, null);
+
         }
 
         public void StartService()
@@ -35,10 +58,16 @@ namespace WPLGSS.Services
             SampleTimer.Elapsed += new System.Timers.ElapsedEventHandler(Sample);
             SampleTimer.Enabled = true;
 
-            dataOut = new byte[2];
+            dataOut = new double[2];
 
             dataOut[0] = 0;
             dataOut[1] = 0;
+        }
+
+        public void SetChannelValue(Channel channel, double value)
+        {
+            if (value == 0) dataOut[channel.ChannelId] = 0;
+            if (value == 1) dataOut[channel.ChannelId] = 12;
         }
     }
 }
--- WPLGSS/Services/LabJackGateway.cs
+++ WPLGSS/Services/LabJackGateway.cs
@@ -11,29 +11,51 @@ namespace WPLGSS.Services
     public class LabJackGateway
     {
         int LJhandle = 0;
+        String[] InputNames;
+        String[] OutputNames;
 
         public int OpenLabJack()
         {
             LJM.OpenS("ANY", "ANY", "ANY", ref LJhandle);
 
+            InputNames[0] = "AIN0";
+            InputNames[0] = "AIN1";
+            InputNames[0] = "AIN2";
+            InputNames[0] = "AIN3";
+            InputNames[0] = "AIN4";
+            InputNames[0] = "AIN5";
+            InputNames[0] = "AIN6";
+            InputNames[0] = "AIN7";
+            InputNames[0] = "AIN8";
+            InputNames[0] = "AIN9";
+            InputNames[0] = "AIN10";
+            InputNames[0] = "AIN11";
+            InputNames[0] = "AIN12";
+            InputNames[0] = "AIN13";
+
+            OutputNames[0] = "DAC0";
+            OutputNames[1] = "DAC1";
+
             return 0;
         }
 
-        public byte[] GetAnalogData(int Channel)
+        public double[] GetAnalogData()
         {
-            byte[] Data = new byte[14];
-            int NumBytes = 0;
+            double[] Data = new double[14];
+            int ErrorAddress = 0;
 
             if (LJhandle == 0) return Data;
 
-            LJM.ReadRaw(LJhandle, Data, NumBytes);
+            LJM.eReadNames(LJhandle, 1, InputNames, Data, ref ErrorAddress);
 
             return Data;
         }
 
-        public void SetAnalogData(byte[] Data, int NumBytes)
+        public void SetAnalogData(double[] Data)
         {
-            LJM.WriteRaw(LJhandle, Data, NumBytes);
+            int ErrorAddress = 0;
+
+            LJM.eWriteNames(LJhandle, 1, OutputNames, Data, ref ErrorAddress);
         }
     }
 }
