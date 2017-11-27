b969c17897bdbadf3792e51ff76ea8d18f1c572e
ysoo <ysoo@wisc.edu>
2017-11-26 21:29:52 -0600

    Sequence runner interface and code stub
    might have left out some stuff will continue working on it

---
+++ WPLGSS/Services/SequenceRunner.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WPLGSS.Models;
+
+namespace WPLGSS.Services
+{
+    public class SequenceRunner: ISequenceRunner
+    {
+        public event EventHandler<StatusChangedEventArgs> SequenceRunningStateChanged;
+        
+
+        public void OnThresholdReached(object threshold, ChannelValueUpdatedEventArgs e)
+        {
+            // Run Abort Sequence
+        }
+
+        public void RunSequence(Sequence sequence)
+        {
+     
+            ObservableCollection<Event> primarysequence = sequence.PrimarySequence;
+            WaitHandle waitObject = null;
+            WaitOrTimerCallback callBack = null;
+            object state = null;
+            TimeSpan timeout;
+            bool executeOnlyOnce = true;
+            foreach (Event curr in primarysequence) {
+                
+                timeout = curr.EndTime - curr.StartTime;
+                RegisteredWaitHandle waitHandle = ThreadPool.RegisterWaitForSingleObject(waitObject, callBack, state, timeout, executeOnlyOnce);
+            }
+
+        }
+    }
+}
