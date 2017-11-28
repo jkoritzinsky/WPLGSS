e1ce576a128e54f0529a45fa7d638e361df68d44
slatts04 <slatts04@gmail.com>
2017-11-27 20:58:47 -0600

    First pass at binding the schematic controls and indicators. Needs work.

--- WPLGSS/ViewModels/SchematicViewModel.cs
+++ WPLGSS/ViewModels/SchematicViewModel.cs
@@ -10,6 +10,8 @@ using System.Threading.Tasks;
 using System.Windows.Input;
 using WPLGSS.Interactivity;
 using WPLGSS.Services;
+using WPLGSS.Models;
+using System.Windows.Controls;
 
 namespace WPLGSS.ViewModels
 {
@@ -17,22 +19,91 @@ namespace WPLGSS.ViewModels
     public class SchematicViewModel : BindableBase
     {
         private readonly IDataAquisition dataAquisition;
+        private readonly IConfigService configService;
 
         [ImportingConstructor]
-        public SchematicViewModel(ISequencePersistence sequencePersistence, ISequenceEditorService fileEditorService, IDataAquisition dataAquisition)
+        public SchematicViewModel(IDataAquisition dataAquisition, IConfigService configService)
         {
-
-            //OpenSequenceCommand = new DelegateCommand(OpenSequence);
-
             this.dataAquisition = dataAquisition;
+            this.configService = configService;
+            dataAquisition.ChannelValueUpdated += (o, e) => PushValueToIndicator(e.Channel, e.Value, e.Time);
         }
 
+        private void PushValueToIndicator(Channel channel, double value, DateTime time)
+        {
+            foreach (ChannelIndicator chi in chIndicators)
+            {
+                if (chi.ch == channel)
+                {
+                    chi.block.Text = value;
+                    break;
+                }
+            }
+        }
         public string Name => "Schematic View";
 
-        //public ICommand StartServiceCommand { get; }
+        struct ChannelIndicator
+        {
+            public Channel ch;
+            public TextBlock block;
+        }
+        List<ChannelIndicator> chIndicators = new List<ChannelIndicator>();
 
+        public void SetBindings(UIElementCollection elements)
+        {
+            string devName;
+            foreach (System.Windows.Controls.UIElement ele in elements)
+            {
+                if (ele is TextBlock)
+                {
+                    // Indicators
 
-        
+                    // Element names are the same as the facility number, but with
+                    // a leading 't' and '-' replaced with '_' to meet XAML naming reqs
+                    devName = (ele as TextBlock).Name.Substring(1).Replace('_', '-');
+
+                    foreach (Channel ch in configService.Config.Channels)
+                    {
+                        if (ch.Name.Equals(devName))
+                        {
+                            ChannelIndicator chi = new ChannelIndicator();
+                            chi.ch = ch;
+                            chi.block = (ele as TextBlock);
+                            chIndicators.Add(chi);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void ToggleOutput(object sender, RoutedEventArgs e)
+        {
+            string name = (sender as System.Windows.Controls.Button).Name.Substring(1).Replace('_', '-');
+            string txt = (sender as System.Windows.Controls.Button).Content;
+            foreach (Channel ch in configService.Config.Channels)
+            {
+                if (!(ch is InputChannel))
+                {
+                    // Output channels only
+                    if (ch.Name.Equals(name))
+                    {
+                        // Match
+                        if (txt.Equals("Open"))
+                        {
+                            dataAquisition.SetChannelValue(ch, 0);
+                            (sender as System.Windows.Controls.Button).Content = "Closed";
+                        }
+                        else if (txt.Equals("Closed"))
+                        {
+                            dataAquisition.SetChannelValue(ch, 1);
+                            (sender as System.Windows.Controls.Button).Content = "Open";
+                        }
+                    }
+                }
+            }
+        }
+
 
         public InteractionRequest<FileInteractionNotification> OpenRequest { get; } = new InteractionRequest<FileInteractionNotification>();
         
